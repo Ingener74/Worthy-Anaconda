3ade20b18fa33333cbd220eea2188589e5f61323
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-08-23 23:44:56 +0400

    урок 5

--- components/RadioOtherOption.jsx
+++ components/RadioOtherOption.jsx
@@ -1,15 +1,30 @@
 var RadioOtherOption = React.createClass({
+    getInitialState: function () {
+        return {
+            checked: false
+        };
+    },
+    onChange: function (event){
+        var input = event.target;
+
+        this.setState({
+            checked: input.checked
+        });
+    },
     render: function () {
         return (
             <p className="form-group radio">
                 <label>
-                    <input type="radio" name="referrer" value="other"/>
+                    <input onChange={this.onChange} type="radio" name="referrer" value="other"/>
                     Other
                 </label>
-                <label>
-                    Please specify:
-                    <input type="text" name="referrer_other"/>
-                </label>
+
+                {this.state.checked && (
+                    <label className="form-inline">
+                        Please specify:
+                        <input type="text" name="referrer_other"/>
+                    </label>
+                )}
             </p>
         );
     }
