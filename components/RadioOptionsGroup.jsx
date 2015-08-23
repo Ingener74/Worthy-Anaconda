a4699d644fd1e04e3acc6b21c7eb1e51e1398583
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-08-23 23:35:18 +0400

    урок 4

--- components/RadioOptionsGroup.jsx
+++ components/RadioOptionsGroup.jsx
@@ -1,16 +1,16 @@
 var RadioOptionGroup = React.createClass({
-   render: function () {
-       return (
+    render: function () {
+        return (
             <div>
-               {this.props.options.map(function(option){
-                   return (
-                       <RadioOption value={option.value} key={option.value}>
-                           {option.label}
-                       </RadioOption>
-                   );
-               })}
-               <RadioOtherOption />
+                {this.props.options.map(function (option) {
+                    return (
+                        <RadioOption value={option.value} key={option.value}>
+                            {option.label}
+                        </RadioOption>
+                    );
+                })}
+                {this.props.other && <RadioOtherOption />}
             </div>
-       );
-   }
+        );
+    }
 });
